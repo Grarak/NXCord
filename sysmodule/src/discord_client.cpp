1a93a7a198bc21bc15a7092e169becf942d91aee
Willi Ye <williye97@gmail.com>
2019-12-12 18:31:01 +0100

    sysmodule: Open connection to websocket

--- sysmodule/src/discord_client.cpp
+++ sysmodule/src/discord_client.cpp
@@ -1,4 +1,8 @@
+#include <switch.h>
+
+#include "base64.h"
 #include "discord_client.h"
+#include "discord_session.h"
 
 ssize_t recv_callback(wslay_event_context_ptr ctx, uint8_t *buf, size_t len,
                       int flags, void *user_data);
@@ -40,6 +44,50 @@ void on_msg_recv_callback(wslay_event_context_ptr ctx,
                           const struct wslay_event_on_msg_recv_arg *arg,
                           void *user_data) {}
 
-bool DiscordClient::connect(const std::string &url) { return false; }
+bool DiscordClient::connect(const std::string &url) {
+  if (!_wslay_event_context) {
+    wslay_event_context_client_init(&_wslay_event_context,
+                                    &_wslay_event_callbacks, this);
+  }
+
+  unsigned char random_bytes[16];
+
+  printf("Generating random key\n");
+  if (R_FAILED(csrngInitialize())) {
+    printf("Failed to initialize csrng\n");
+    return false;
+  }
+
+  if (R_FAILED(csrngGetRandomBytes(random_bytes, 16))) {
+    printf("Failed to generate random bytes\n");
+    csrngExit();
+    return false;
+  }
+
+  std::string random_key = base64_encode(random_bytes, 16);
+
+  DiscordSession session;
+  session.setUrl(url);
+
+  std::vector<SleepyDiscord::HeaderPair> header = {
+      {"Upgrade", "websocket"},
+      {"Connection", "Upgrade"},
+      {"Sec-WebSocket-Key", random_key},
+      {"Sec-WebSocket-Version", "13"}};
+  session.setHeader(header);
+
+  printf("Request conenction to websocket\n");
+  SleepyDiscord::Response response;
+  _mbedtls_wrapper = session.request(SleepyDiscord::Get, &response);
+
+  if (response.statusCode !=
+      SleepyDiscord::SWITCHING_PROTOCOLS) {  // error check
+    printf("Websocket connection Error: %s\n", response.text.c_str());
+    return false;
+  }
+
+  printf("Connection to websocket established\n");
+  return true;
+}
 
 void DiscordClient::tick() {}
--- sysmodule/src/discord_client.h
+++ sysmodule/src/discord_client.h
@@ -2,11 +2,15 @@
 #include <sleepy_discord/sleepy_discord.h>
 #include <wslay/wslay.h>
 
+#include "mbedtls_wrapper.h"
+
 class DiscordClient : public SleepyDiscord::BaseDiscordClient {
  private:
   std::string _token;
   wslay_event_context_ptr _wslay_event_context;
   wslay_event_callbacks _wslay_event_callbacks;
+
+  std::shared_ptr<MBedTLSWrapper> _mbedtls_wrapper;
 
   bool connect(const std::string&) override;
 
