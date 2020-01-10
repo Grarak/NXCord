299d82ca13625280908e71b1f2e21c21667f785e
Willi Ye <williye97@gmail.com>
2020-01-10 22:32:03 +0100

    client: Define author and version * Refactor main

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -8,72 +8,8 @@
 #include "ui/ui_main.hpp"
 
 extern "C" {
-void userAppInit(void) {
-#ifndef APPLICATION
-  // Seems like every thread on the switch needs to sleep for a little
-  // or it will block the entire console
-  // Specifically in Kosmos Toolbox's case, you need to wait about 0.2 sec
-  // or it won't let you turn it on/off the sysmodule after a few tries
-  svcSleepThread(2e+8L);
-#endif
-
-  Result rc = smInitialize();
-  if (R_FAILED(rc)) {
-    fatalThrow(rc);
-  }
-
-#ifdef STANDALONE
-  SocketInitConfig sockConf = {
-      .bsdsockets_version = 1,
-
-      .tcp_tx_buf_size = 0x800,
-      .tcp_rx_buf_size = 0x1000,
-      .tcp_tx_buf_max_size = 0x2EE0,
-      .tcp_rx_buf_max_size = 0x2EE0,
-
-      .udp_tx_buf_size = 0x800,
-      .udp_rx_buf_size = 0x1000,
-
-      .sb_efficiency = 4,
-  };
-  rc = socketInitialize(&sockConf);
-  if (R_FAILED(rc)) {
-    fatalThrow(rc);
-  }
-
-  rc = csrngInitialize();
-  if (R_FAILED(rc)) {
-    fatalThrow(rc);
-  }
-
-  rc = audoutInitialize();
-  if (R_FAILED(rc)) {
-    fatalThrow(rc);
-  }
-
-  rc = audinInitialize();
-  if (R_FAILED(rc)) {
-    fatalThrow(rc);
-  }
-
-  nxlinkStdio();
-#endif
-}
-
-void userAppExit(void) {
-  Logger::write("Closing services\n");
-#ifdef STANDALONE
-  audinExit();
-  audoutExit();
-  csrngExit();
-  socketExit();
-#endif
-  smExit();
-}
-
 alignas(16) u8 __nx_exception_stack[0x1000];
 u64 __nx_exception_stack_size = sizeof(__nx_exception_stack);
-__attribute__((weak)) u32 __nx_exception_ignoredebug = 1;
 
 void __libnx_exception_handler(ThreadExceptionDump *ctx) {
   Logger::write("Crashed with error 0x%x\n", ctx->error_desc);
@@ -101,6 +37,43 @@ std::string_view log_name = "nxcord-client";
 
 int main(int argc, char *argv[]) {
   {
+#ifdef STANDALONE
+    SocketInitConfig sockConf = {
+        .bsdsockets_version = 1,
+
+        .tcp_tx_buf_size = 0x800,
+        .tcp_rx_buf_size = 0x1000,
+        .tcp_tx_buf_max_size = 0x2EE0,
+        .tcp_rx_buf_max_size = 0x2EE0,
+
+        .udp_tx_buf_size = 0x800,
+        .udp_rx_buf_size = 0x1000,
+
+        .sb_efficiency = 4,
+    };
+    Result rc = socketInitialize(&sockConf);
+    if (R_FAILED(rc)) {
+      fatalThrow(rc);
+    }
+
+    rc = csrngInitialize();
+    if (R_FAILED(rc)) {
+      fatalThrow(rc);
+    }
+
+    rc = audoutInitialize();
+    if (R_FAILED(rc)) {
+      fatalThrow(rc);
+    }
+
+    rc = audinInitialize();
+    if (R_FAILED(rc)) {
+      fatalThrow(rc);
+    }
+
+    nxlinkStdio();
+#endif
+
     std::shared_ptr<NXCordComInterface> interface = std::make_shared<
 #ifdef STANDALONE
         StandaloneClient
@@ -118,6 +91,12 @@ int main(int argc, char *argv[]) {
     main->Show();
   }
 
-  userAppExit();
+  Logger::write("Closing services\n");
+#ifdef STANDALONE
+  audinExit();
+  audoutExit();
+  csrngExit();
+  socketExit();
+#endif
   return 0;
 }
